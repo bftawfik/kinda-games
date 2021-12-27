2ca44fdc53d681501527a5eb748ecbc1c8309a1f
Bishoy Farouk <bftawfik@gmail.com>
2021-12-27 13:06:30 +0200

    use local server on development

--- src/Services/requests.ts
+++ src/Services/requests.ts
@@ -3,40 +3,52 @@ const server: string = "94.237.109.240";
 const port: string = "4200";
 const loginPort: string = "4000";
 
+const localServer = "localhost";
+
 const postIsSubscribed = async (token: string) =>
-  await fetch(`${protocol}://${server}:${port}/tpay/isSubscribed`, {
-    method: "POST",
-    mode: "cors",
-    cache: "no-cache",
-    credentials: "same-origin",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    redirect: "follow",
-    referrerPolicy: "no-referrer",
-    body: JSON.stringify({ token: token }),
-  });
+  await fetch(
+    !process.env.NODE_ENV || process.env.NODE_ENV === "development"
+      ? `${protocol}://${localServer}:${port}/tpay/isSubscribed`
+      : `${protocol}://${server}:${port}/tpay/isSubscribed`,
+    {
+      method: "POST",
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      redirect: "follow",
+      referrerPolicy: "no-referrer",
+      body: JSON.stringify({ token: token }),
+    }
+  );
 
 const postCenterLogin = async (
   msisdn: string,
   serviceName: string,
   language: number
 ) =>
-  await fetch(`${protocol}://${server}:${loginPort}/center/login`, {
-    method: "POST",
-    mode: "cors",
-    cache: "no-cache",
-    credentials: "same-origin",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    redirect: "follow",
-    referrerPolicy: "no-referrer",
-    body: JSON.stringify({
-      msisdn: msisdn,
-      service: serviceName,
-      language: language,
-    }),
-  });
+  await fetch(
+    !process.env.NODE_ENV || process.env.NODE_ENV === "development"
+      ? `${protocol}://${localServer}:${loginPort}/center/login`
+      : `${protocol}://${server}:${loginPort}/center/login`,
+    {
+      method: "POST",
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      redirect: "follow",
+      referrerPolicy: "no-referrer",
+      body: JSON.stringify({
+        msisdn: msisdn,
+        service: serviceName,
+        language: language,
+      }),
+    }
+  );
 
 export { postIsSubscribed, postCenterLogin };
